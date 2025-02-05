f683cdeb470eb2a3e2c7282c50e9161f82ddeecc
Madleyym <tryandaasu@gmail.com>
2025-02-06 03:48:01 +0700

    Comment out footer section and related styles in footer.php

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,11 +1,12 @@
-<footer class="footer mt-auto py-3 bg-light">
+<!-- <footer class="footer mt-auto py-3 bg-light">
     <div class="container text-center">
         <span class="text-muted">© <?= date('Y') ?> <?= APP_NAME ?>. All rights reserved.</span>
     </div>
 </footer>
 
 <style>
-    html, body {
+    html,
+    body {
         height: 100%;
         margin: 0;
         display: flex;
@@ -18,10 +19,13 @@
 
     .footer {
         width: 100%;
-        background-color: #f8f9fa; /* Warna footer */
+        /* background-color: #f8f9fa; */
+        /* Warna footer */
         position: relative;
-        bottom: 0;
+        /* bottom: 0; */
         padding: 15px 0;
         text-align: center;
+        /* margin-top: auto; */
+
     }
-</style>
+</style> -->
